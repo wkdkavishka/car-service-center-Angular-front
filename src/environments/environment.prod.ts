2de2149f584c3433bae5a08143e641e6b47c7cf8
wkdkavishka <w.k.d.kavishka@outlook.com>
2025-01-16 00:48:55 +0530

    FIX cors problem

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://localhost:4000/car_service/',
-  apiUrl: 'http://13.211.69.158:4000/car_service/',
+  // apiUrl: 'http://127.0.0.1:4000/car_service/',
+  apiUrl:
+    'http://ec2-13-211-69-158.ap-southeast-2.compute.amazonaws.com:4000/car_service/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:4000/car_service/',
-  // apiUrl: 'http://13.211.69.158:4000/car_service/',
 };
